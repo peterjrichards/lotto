2c91402ca485602a1afb91ec32d1ae0f7cd39ba8
Peter Richards <peter.richards@thefuturetoday.net>
2021-01-04 20:32:57 +0000

    Generate set of random numbers. First test is passing

--- LottoService/LottoService.cs
+++ LottoService/LottoService.cs
@@ -5,9 +5,26 @@ namespace LottoService
 {
   public class LottoService : ILottoService
   {
+    protected readonly Random _random;
+
+    public LottoService()
+    {
+      _random = new Random();
+    }
+
     public IEnumerable<int> GenerateNumbers(int count = 6, int min = 1, int max = 49)
     {
-      throw new NotImplementedException();
+      var numbers = new HashSet<int>();
+      while (numbers.Count < count)
+      {
+        var num = _random.Next(min, max);
+        // Check and exclude duplicate
+        if (!numbers.Contains(num))
+        {
+          numbers.Add(num);
+        }
+      }
+      return numbers;
     }
   }
 }
